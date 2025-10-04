c516696f1e981497cea429d182c0e71131de7a97
INovikov Home <nov.ilucha@yandex.ru>
2025-10-04 12:53:50 +0300

    add facts table schema

---
+++ src/schemas/facts.py
@@ -0,0 +1,41 @@
+import uuid
+
+from sqlalchemy import (
+    UUID,
+    Column,
+    ForeignKeyConstraint,
+    Integer,
+    Numeric,
+    PrimaryKeyConstraint,
+    Table,
+)
+
+from src.schemas.metadata import metadata_obj
+
+facts = Table(
+    "facts",
+    metadata_obj,
+    Column("id", UUID(as_uuid=True), default=uuid.uuid4),
+    Column("fk_id_city", UUID(as_uuid=True), nullable=True, comment="FK ID Город"),
+    Column(
+        "fk_id_customer", UUID(as_uuid=True), nullable=True, comment="FK ID Покупатель"
+    ),
+    Column("fk_id_product", UUID(as_uuid=True), nullable=True, comment="FK ID Продукт"),
+    Column("fk_id_date", UUID(as_uuid=True), nullable=True, comment="FK ID Дата"),
+    Column("price", Numeric(10, 2), nullable=True, comment="Цена"),
+    Column("volume", Integer, nullable=True, comment="Количество"),
+    Column("amount_of_money", Numeric(10, 2), nullable=True, comment="Сумма"),
+    Column("remains", Numeric(10, 2), nullable=True, comment="Остаток"),
+    Column("debt", Numeric(10, 2), nullable=True, comment="Задолженность"),
+    # связи ключей
+    ForeignKeyConstraint(
+        ["fk_id_product"],
+        ["product.id_product"],
+        name="fk_facts_product_id_product",
+        onupdate="CASCADE",
+        ondelete="SET NULL",
+    ),
+    # ограничения на колонки
+    PrimaryKeyConstraint("id", name="pk_facts_id"),
+    comment="Факты",
+)

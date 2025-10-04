1e6da549bc4f69911c4d32ba3be80372b37a81fa
INovikov Home <nov.ilucha@yandex.ru>
2025-10-04 12:54:54 +0300

    link facts into package

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,4 +1,5 @@
 from src.schemas.metadata import metadata_obj
 from src.schemas import product
+from src.schemas import facts
 
-__all__ = ["metadata_obj", "product"]
+__all__ = ["metadata_obj", "product", "facts"]
